2a1edd5f0778f9464ba4d1fda1d81e7cfc599032
tabrezhere <tabrezhere@gmail.com>
2020-11-24 21:39:22 +0530

    new file updloaded

---
+++ App_Code/global.acax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for global
+/// </summary>
+public class global
+{
+	public global()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+
+	}
+}
